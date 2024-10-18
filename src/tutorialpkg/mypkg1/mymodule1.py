090cb57c80e9e566f943852ab4c7fa3c518c5c0b
MGWWT <zceigxu@ucl.ac.uk>
2024-10-18 15:05:04 +0100

    10.18

--- src/tutorialpkg/mypkg1/mymodule1.py
+++ src/tutorialpkg/mypkg1/mymodule1.py
@@ -5,6 +5,9 @@ mock_database = {
 
 if __name__ == '__main__':
     # The functions are in the modules in mypkg2. You will need to import them.
+    import tutorialpkg
+    from tutorialpkg.mypkg2.mymodule2_1 import calculate_area_of_circle
+    from tutorialpkg.mypkg2.mymodule2_2 import fetch_user_data
 
     # Calculate the area of a circle with a radius of 10. Print the result.
     area = calculate_area_of_circle(10)
@@ -13,4 +16,14 @@ if __name__ == '__main__':
     # Use the fetch_user_data(user_id, database) function to print the data for the user with ID 42 that is in `mock_database` variable.
     print(fetch_user_data(42, mock_database))
 
-    # Locate the data file `paralmpics_raw.csv` relative to this file using pathlib.Path. Prove it exists.
+# Locate the data file `paralmpics_raw.csv` relative to this file using pathlib.Path. Prove it exists.
+from pathlib import Path
+
+# This script is located in a subfolder so you need to navigate up to the parent (src) and then its parent (project root), then down to the 'data' directory and finally the .csv file
+csv_file = Path('src/tutorialpkg/mypkg/mymodule1.py').parent.parent.joinpath('data', 'paralympics_events_raw.csv')
+
+# Check if the file exists
+if csv_file.exists():
+    print(f"CSV file found: {csv_file}")
+else:
+    print("CSV file not found.")
